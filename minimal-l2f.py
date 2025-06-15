718270d108270cab266ea84add2a934c3f76ab41
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-06-15 18:35:07 -0400

    fixing things but still not great

--- minimal-l2f.py
+++ minimal-l2f.py
@@ -4,7 +4,9 @@ import struct
 import numpy as np
 import pygame
 import time
-from functools import reduce
+import websockets
+import json
+from scipy.spatial.transform import Rotation as R
 
 from copy import copy
 import l2f
@@ -22,6 +24,7 @@ policy = QuadrotorPolicy()
 device = l2f.Device()
 rng = vector.VectorRng()
 env = vector.VectorEnvironment()
+ui = l2f.UI()
 params = vector.VectorParameters()
 state = vector.VectorState()
 next_state = vector.VectorState()
@@ -48,6 +51,9 @@ gamepad_mapping = {
 }
 
 betaflight_order = ["roll", "pitch", "throttle", "yaw", "arm"] # AETR
+# crazyflie: top-right, bottom-right, bottom-left, top-left
+# betaflight: bottom-right, top-right, bottom-left, top-left, 
+crazyflie_from_betaflight_motors = [1, 0, 2, 3]
 
 initial_axes = None
 def test_rc_channels():
@@ -93,11 +99,15 @@ def parse_rpm_packet(data):
         return struct.unpack('<4f', data[:16])
     return [0.0, 0.0, 0.0, 0.0]
 
-def make_fdm_packet(state, drone_id=0):
+def flu_to_frd(vec):
+    if len(vec) == 3:
+        return np.array([vec[0], -vec[1], -vec[2]], dtype=np.float64)
+    elif len(vec) == 4: # Quaternion (w, x, y, z)
+        return np.array([vec[0], vec[1], -vec[2], -vec[3]], dtype=np.float64)
+def make_fdm_packet(state, accelerometer, drone_id=0):
     s = state.states[drone_id]
     timestamp = time.time()
     imu_angular_velocity_rpy = np.asarray(s.angular_velocity, dtype=np.float64)
-    imu_linear_acceleration_xyz = np.zeros(3, dtype=np.float64) # todo
     imu_orientation_quat = np.asarray(s.orientation, dtype=np.float64)
     velocity_xyz = np.asarray(s.linear_velocity, dtype=np.float64)
     position_xyz = np.asarray(s.position, dtype=np.float64)
@@ -105,11 +115,11 @@ def make_fdm_packet(state, drone_id=0):
     fmt = '<d3d3d4d3d3dd'
     packet = struct.pack(fmt,
         timestamp,
-        *imu_angular_velocity_rpy,
-        *imu_linear_acceleration_xyz,
-        *imu_orientation_quat,
-        *velocity_xyz,
-        *position_xyz,
+        *flu_to_frd(imu_angular_velocity_rpy),
+        *flu_to_frd(accelerometer),
+        *flu_to_frd(imu_orientation_quat),
+        *flu_to_frd(velocity_xyz),
+        *flu_to_frd(position_xyz),
         pressure
     )
     return packet
@@ -118,46 +128,84 @@ async def udp_recv(loop, sock):
     return await loop.sock_recv(sock, 1024)
 
 async def main():
-    loop = asyncio.get_running_loop()
-    udp_pwm_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_pwm_sock.bind((UDP_IP, PORT_PWM))
-    udp_pwm_sock.setblocking(False)
-    udp_state_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_rc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    uri = "ws://localhost:13337/backend"
+    async with websockets.connect(uri) as websocket:
+        handshake = json.loads(await websocket.recv(uri))
+        assert(handshake["channel"] == "handshake")
+        namespace = handshake["data"]["namespace"]
+        ui.ns = namespace
+        ui_message = vector.set_ui_message(device, env, ui)
+        parameters_message = vector.set_parameters_message(device, env, params, ui)
+        await websocket.send(ui_message)
+        await websocket.send(parameters_message)
 
-    policy.reset()
-    armed = False
-    while True:
-        rc_channels = get_rc_channels()
-        timestamp = time.time()
-        rc_packet = struct.pack(f'<d{SIMULATOR_MAX_RC_CHANNELS}h', timestamp, *rc_channels)
-        udp_rc_sock.sendto(rc_packet, (UDP_IP, PORT_RC))
+        loop = asyncio.get_running_loop()
+        udp_pwm_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_pwm_sock.bind((UDP_IP, PORT_PWM))
+        udp_pwm_sock.setblocking(False)
+        udp_state_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_rc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        if rc_channels[betaflight_order.index("arm")] > 1500:
-            policy.reset()
-            vector.initial_state(device, env, params, state)
-            armed = True
+        policy.reset()
+        armed = False
+        simulation_dts = []
+        previous_time = time.time()
+        while True:
+            rc_channels = get_rc_channels()
+            timestamp = time.time()
+            rc_packet = struct.pack(f'<d{SIMULATOR_MAX_RC_CHANNELS}h', timestamp, *rc_channels)
+            udp_rc_sock.sendto(rc_packet, (UDP_IP, PORT_RC))
 
-        try:
-            data = await asyncio.wait_for(udp_recv(loop, udp_pwm_sock), timeout=0.01)
-            rpms = parse_rpm_packet(data)
-        except asyncio.TimeoutError:
-            rpms = [0.0, 0.0, 0.0, 0.0]
-        
-        vector.observe(device, env, params, state, observation, rng)
-        action = policy.evaluate_step(observation[:, :22])
-        action[0] = np.array(rpms) * 2 - 1
-        dts = vector.step(device, env, params, state, action, next_state, rng)
-        if armed:
-            state.assign(next_state)
+            if rc_channels[betaflight_order.index("arm")] > 1500:
+                if not armed:
+                    policy.reset()
+                    vector.initial_state(device, env, params, state)
+                armed = True
 
-        state_packet = make_fdm_packet(state)
-        udp_state_sock.sendto(state_packet, (UDP_IP, PORT_STATE))
+            try:
+                data = await asyncio.wait_for(udp_recv(loop, udp_pwm_sock), timeout=0.01)
+                rpms = parse_rpm_packet(data)
+            except asyncio.TimeoutError:
+                rpms = [0.0, 0.0, 0.0, 0.0]
+
+            simulation_dt = time.time() - previous_time
+            previous_time = time.time()
+            simulation_dts.append(simulation_dt)
+            simulation_dts = simulation_dts[-100:]
+
+            for i in range(env.N_ENVIRONMENTS):
+                parameters_string = l2f.parameters_to_json(device, env.environments[i], params.parameters[i])
+                parameters = json.loads(parameters_string)
+                parameters["integration"]["dt"] = simulation_dt
+                l2f.parameters_from_json(device, env.environments[i], json.dumps(parameters), params.parameters[i])
+            
+            vector.observe(device, env, params, state, observation, rng)
+            action = policy.evaluate_step(observation[:, :22])
+            action[0] = np.array(rpms)[crazyflie_from_betaflight_motors] * 2 - 1
+            dts = vector.step(device, env, params, state, action, next_state, rng)
+            acceleration = (next_state.states[0].linear_velocity - state.states[0].linear_velocity) / simulation_dt
+            r = R.from_quat([*state.states[0].orientation[1:], state.states[0].orientation[0]])
+            R_wb = r.as_matrix()
+            accelerometer = R_wb.T @ (acceleration - np.array([0, 0, -9.81], dtype=np.float64))
+            if armed:
+                state.assign(next_state)
+            
+            state.states[0].position[:] = 0
+            state.states[0].linear_velocity[:] = 0
+
+            ui_state = copy(state)
+            for i, s in enumerate(ui_state.states):
+                s.position[0] += i * 0.1 # Spacing for visualization
+            state_action_message = vector.set_state_action_message(device, env, params, ui, ui_state, action)
+            await websocket.send(state_action_message)
+
+            state_packet = make_fdm_packet(state, accelerometer)
+            udp_state_sock.sendto(state_packet, (UDP_IP, PORT_STATE))
 
 
-        print(f"RPMs: {rpms} ")
-        await asyncio.sleep(dts[-1] if hasattr(dts, '__getitem__') else 0.01)
-        # test_rc_channels()
+            print(f"RPMs: {rpms} dt: {np.mean(simulation_dts):.4f} s, action: {action[0].tolist()}")
+            # await asyncio.sleep(dts[-1] if hasattr(dts, '__getitem__') else 0.01)
+            # test_rc_channels()
 
 if __name__ == "__main__":
     asyncio.run(main())
