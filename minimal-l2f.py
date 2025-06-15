da9fe7516071bd1e4ff227ca72ad70cdff7dae9e
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-06-15 16:04:55 -0400

    back to sim

--- minimal-l2f.py
+++ minimal-l2f.py
@@ -2,14 +2,18 @@ from copy import copy
 import numpy as np
 import asyncio, websockets, json
 import l2f
-from l2f import vector2 as vector
+from l2f import vector8 as vector
+from foundation_model import QuadrotorPolicy
 
+policy = QuadrotorPolicy()
 device = l2f.Device()
 rng = vector.VectorRng()
 env = vector.VectorEnvironment()
 ui = l2f.UI()
 params = vector.VectorParameters()
 state = vector.VectorState()
+observation = np.zeros((env.N_ENVIRONMENTS, env.OBSERVATION_DIM), dtype=np.float32)
+next_state = vector.VectorState()
 
 vector.initialize_rng(device, rng, 0)
 vector.initialize_environment(device, env)
@@ -27,17 +31,23 @@ async def main():
         parameters_message = vector.set_parameters_message(device, env, params, ui)
         await websocket.send(ui_message)
         await websocket.send(parameters_message)
+        policy.reset()
         for _ in range(500):
-            action = np.ones((env.N_ENVIRONMENTS, env.ACTION_DIM), dtype=np.float32) * -1
+            vector.observe(device, env, params, state, observation, rng)
+            action = policy.evaluate_step(observation[:, :22])
+            dts = vector.step(device, env, params, state, action, next_state, rng)
+            state.assign(next_state)
             ui_state = copy(state)
             for i, s in enumerate(ui_state.states):
-                s.position[0] += i * 0.1
+                s.position[0] += i * 0.1 # Spacing for visualization
             state_action_message_string = vector.set_state_action_message(device, env, params, ui, ui_state, action)
             state_action_message = json.loads(state_action_message_string)
             state_action_message["data"]["state"][0]["position"] = [0.0, 0.0, 0.0]
             state_action_message["data"]["state"][0]["orientation"] = [1.0, 0.0, 0.0, 0.0]
             await websocket.send(json.dumps(state_action_message))
-            await asyncio.sleep(0.01)
+            await asyncio.sleep(dts[-1])
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
