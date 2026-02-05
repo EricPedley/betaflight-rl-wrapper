a8f045a4f36e848e5dcb207c358885c97c7567e7
Eric Pedley <ericpedley@gmail.com>
2026-02-05 09:24:05 -0800

    fix more dumbass firmware bugs

--- src/rl_tools/policy.h
+++ src/rl_tools/policy.h
@@ -2,7 +2,6 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    void rl_tools_status(void);
     void rl_tools_control(bool);
     extern float rl_tools_position[3];
     extern float rl_tools_linear_velocity[3];
